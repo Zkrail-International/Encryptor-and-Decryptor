8b2b1aa3c012184a2f7ba9142662264ae84a7a07
Zkrail-International <121692537+Zkrail-International@users.noreply.github.com>
2023-02-12 16:13:07 -0800

    fix bug

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import enc
+import os
+
+cwd = os.getcwd()
 
 while True:
     ask = input('Welcome to Zkrail\'s Encryptor.\n\nWould you like to, [encrypt] or [decrypt]?\n\n')
+    os.chdir(cwd)
     if ask == 'encrypt':
         enc.data.askencryption()
     elif ask == 'decrypt':
